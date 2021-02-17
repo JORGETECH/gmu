f9b356524de3b003ab68f66c36014c75be713d2e
JORGETECH <jorgesanini@gmail.com>
2021-02-17 19:50:21 +0100

    Adapted config.mk for newer buildroot (by pingflood)
    I was using the buildroot mentioned in RetroFW's wiki before but now I'm using the latest pingflood's buildroot copied over to the /opt/rs97tools directory (default directory for the toolchain if you follow the RetroFW's wiki instructions).
    
    I also removed the ncursesw include line since we are not using the ncurses frontend on RetroFW and ncursesw is missing on the buildroot anyways.

--- config.mk
+++ config.mk
@@ -1,9 +1,9 @@
 # Generated Gmu config
 SDLFE_WITHOUT_SDL_GFX=1
 CC=mipsel-linux-gcc
-SDL_CFLAGS=-I/opt/buildroot-2018.02.9/output/host/mipsel-buildroot-linux-uclibc/sysroot/usr/include/SDL -D_GNU_SOURCE=1 -D_REENTRANT
-SDL_LIB=-L/opt/buildroot-2018.02.9/output/host/mipsel-buildroot-linux-uclibc/sysroot/usr/lib -lSDL -lpthread
-CFLAGS= -I/opt/buildroot-2018.02.9/output/host/mipsel-buildroot-linux-uclibc/sysroot/usr/include $(SDL_CFLAGS) -fsigned-char -D_REENTRANT -DUSE_MEMORY_H  -I/usr/bin/../include/ncursesw -DFILE_HW_H="\"hw_dingux.h\""
+SDL_CFLAGS=-I/opt/rs97tools/mipsel-RetroFW-linux-uclibc/sysroot/usr/include/SDL -D_GNU_SOURCE=1 -D_REENTRANT
+SDL_LIB=-L/opt/rs97tools/mipsel-RetroFW-linux-uclibc/sysroot/usr/lib -lSDL -lpthread
+CFLAGS= -I/opt/rs97tools/mipsel-RetroFW-linux-uclibc/sysroot/usr/include $(SDL_CFLAGS) -fsigned-char -D_REENTRANT -DUSE_MEMORY_H  -DFILE_HW_H="\"hw_dingux.h\""
 LFLAGS=   -Wl,-export-dynamic
 LFLAGS_CORE=
 LIBS_CORE= -ldl
@@ -12,11 +12,11 @@ FRONTENDS_TO_BUILD= frontends/sdl.so frontends/log.so
 TARGET=dingux
 TOOLS_TO_BUILD= gmu.bin
 
-DEC_opus_CFLAGS=-I/opt/buildroot-2018.02.9/output/host/mipsel-buildroot-linux-uclibc/sysroot/usr/include/opus
+DEC_opus_CFLAGS=-I/opt/rs97tools/mipsel-RetroFW-linux-uclibc/sysroot/usr/include/opus
 
 DISTFILES          = \
                      $(BINARY) frontends decoders themes \
                      libs.dingux \
-                     gmu.bmp gmu.png README.txt COPYING \
+                     gmu.png README.txt COPYING \
                      htdocs \
                      gmu.dge gmuinput.dingux.conf \
