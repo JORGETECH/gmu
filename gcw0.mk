c80e094a7f7c22ecb72d470813b7f2ea7d6b0ed5
JORGETECH <jorgesanini@gmail.com>
2021-03-15 12:38:48 +0100

    Add gcw0.mk file

---
+++ gcw0.mk
@@ -0,0 +1,60 @@
+#
+# Gmu Music Player
+#
+# Copyright (c) 2013-2018 Denis Kuznetsov (denis.n.kuznetsov@gmail.com)
+#
+# File: gcw0.mk  Created: 131018
+#
+# Description: Makefile configuration (GCW Zero)
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; version 2 of
+# the License. See the file COPYING in the Gmu's main directory
+# for details.
+#
+
+DECODERS_TO_BUILD  = \
+                     decoders/vorbis.so \
+                     decoders/flac.so \
+                     decoders/modplug.so \
+                     decoders/mpg123.so \
+		     decoders/opus.so \
+		     decoders/speex.so \
+		     decoders/wavpack.so \
+#		     decoders/musepack.so
+
+FRONTENDS_TO_BUILD = \
+                     frontends/sdl.so \
+                     frontends/gmuhttp.so \
+
+GCW0_SDK          ?= /opt/RG350_buildroot/output/host/
+SYSROOT_BIN        = usr/mipsel-gcw0-linux-uclibc/sysroot/usr/bin/
+TOOLS_TO_BUILD     =
+SDL_LIB            = `$(GCW0_SDK)$(SYSROOT_BIN)sdl-config --libs`
+SDL_CFLAGS         = `$(GCW0_SDK)$(SYSROOT_BIN)sdl-config --cflags`
+CXX                = $(GCW0_SDK)usr/bin/mipsel-linux-g++
+CC                 = $(GCW0_SDK)usr/bin/mipsel-linux-gcc
+STRIP              = $(GCW0_SDK)usr/bin/mipsel-linux-strip
+
+COPTS             ?= -O2
+
+CFLAGS             = \
+                     $(SDL_CFLAGS) \
+                     -fomit-frame-pointer -ffunction-sections -ffast-math \
+                     -I$(GCW0_SDK)/usr/mipsel-gcw0-linux-uclibc/sysroot/usr/include/ \
+                     -DFILE_HW_H="\"hw_gcw0.h\"" \
+
+LFLAGS             = \
+                     $(SDL_LIB) \
+                     -lm -ldl -lz -lgcc -Wl,-export-dynamic \
+
+DEC_opus_CFLAGS=-I$(GCW0_SDK)/usr/mipsel-gcw0-linux-uclibc/sysroot/usr/include/opus
+
+DISTFILES          = \
+                     $(BINARY) frontends decoders themes \
+                     libs.gcw0 \
+                     gmu.bmp gmu.png README.txt COPYING \
+                     htdocs \
+                     gmu-gcw0.dge gmuinput.gcw0.conf default.gcw0.desktop \
+
